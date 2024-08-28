d3bc5a2d94e21585476582aa30062aa39a77918c
Heecheon Park <heecheon@thesesamelab.com>
2024-08-28 09:24:47 +0900

    semi-ported Observation framework from Swift (naive implementation)

--- CaseStudy/observation.ts
+++ CaseStudy/observation.ts
@@ -26,19 +26,19 @@ class ObservationRegistrar {
   observations: Record<ObservationID, Observation> = {};
 
   access<TSubject extends Observable>(
-    subject: TSubject,
+    _subject: TSubject,
     keyPath: KeyPath<TSubject>
   ) {
     if (GLOBAL_ACCESS_LIST) GLOBAL_ACCESS_LIST.trackAccess(this, keyPath);
-    console.log(`access(${keyPath})`);
+    console.log(`access(${keyPath}) [${Object.keys(this.lookups)}]`);
   }
 
   withMutation<TSubject, TMutated>(
-    subject: TSubject,
+    _subject: TSubject,
     keyPath: KeyPath<TSubject>,
     mutate: () => TMutated
   ): Maybe<TMutated> {
-    console.log(`withMutation(${keyPath})`);
+    console.log(`withMutation(${keyPath}) [${Object.keys(this.lookups)}]`);
     const observationIds = this.lookups[keyPath];
     if (observationIds) {
       for (const observationId of observationIds) {
@@ -46,13 +46,10 @@ class ObservationRegistrar {
         if (observation) {
           observation.closure();
           for (const eachKeyPath of observation.keyPaths) {
-            // this.lookups[eachKeyPath]?.delete(observation.id);
+            this.lookups[eachKeyPath]?.delete(observation.id);
           }
-        } else console.log("withMutation no observation");
+        }
       }
-    } else {
-      // console.log(`withMutation no observationIds ${JSON.stringify(this.lookups, null, 2)}`)
-      console.log(`withMutation no observationIds ${this.lookups}`);
     }
 
     try {
@@ -66,10 +63,10 @@ class ObservationRegistrar {
     const observation = this.observations[observationId];
     if (observation) {
       for (const keyPath of observation.keyPaths) {
-        // this.lookups[keyPath].delete(observation.id);
-        // if (!this.lookups[keyPath].size) delete this.lookups[keyPath];
+        this.lookups[keyPath].delete(observation.id);
+        if (!this.lookups[keyPath].size) delete this.lookups[keyPath];
       }
-      // delete this.observations[observationId];
+      delete this.observations[observationId];
     }
   }
 
@@ -77,20 +74,10 @@ class ObservationRegistrar {
     const observation = new Observation(keyPaths, onChange);
     this.observations[observation.id] = observation;
     for (const keyPath of keyPaths) {
-      console.log(`keyPath: ${keyPath}`);
-      if (this.lookups[keyPath]) {
-        this.lookups[keyPath].add(observation.id);
-        console.log(
-          `registerOnChange on ObservationRegistrar called ${Array.from(
-            this.lookups[keyPath]
-          )}`
-        );
-      } else {
-        console.log(`lookups missing`);
-        for (const key in this.lookups) {
-          console.log(`lookups missing: ${key}`);
-        }
+      if (!this.lookups[keyPath]) {
+        this.lookups[keyPath] = new Set();
       }
+      this.lookups[keyPath].add(observation.id);
     }
     return observation.id;
   }
@@ -110,10 +97,8 @@ class AccessList {
     let id = UUID();
     if (!this.entries[id]) this.entries[id] = new Entry(r);
     this.entries[id].keyPaths.add(kp);
-    console.log(`AccessList entries ${Array.from(this.entries[id].keyPaths)}`);
   }
   registerOnChange(thunk: () => void) {
-    console.log("registerOnChange on AccessList called");
     const observationIds: Record<ObjectIdentifier, ObservationID> = {};
     const processThunk = () => {
       thunk();
@@ -126,13 +111,29 @@ class AccessList {
     for (const [key, entry] of Object.entries(this.entries)) {
       const registrar = entry.registrar;
       const keyPaths = entry.keyPaths;
-      console.log(`AccessList keypaths ${Array.from(keyPaths)}`);
       const observationId = registrar.registerOnChange(keyPaths, processThunk);
       observationIds[key as ObjectIdentifier] = observationId;
     }
   }
 }
 
+function withObservationTracking<T>(apply: () => T, onChange: () => void): T {
+  GLOBAL_ACCESS_LIST = new AccessList();
+  const result = apply();
+  GLOBAL_ACCESS_LIST?.registerOnChange(onChange);
+  return result;
+}
+
+/**
+ * A decorator function that simulates SwiftUI's @Observable macro in TypeScript.
+ * It wraps a class to automatically observe property changes and notify
+ * the `ObservationRegistrar`. This is designed to closely mimic the behavior of
+ * SwiftUI's `@Observable` macro.
+ *
+ * @template T - The type of the class being decorated.
+ * @param {T} constructor - The constructor of the class being decorated.
+ * @returns {T} A new class that extends the original class with observation functionality.
+ */
 function Observable<T extends { new (...args: any[]): object }>(
   constructor: T
 ) {
@@ -176,35 +177,35 @@ function Observable<T extends { new (...args: any[]): object }>(
   };
 }
 
-function withObservationTracking<T>(apply: () => T, onChange: () => void): T {
-  GLOBAL_ACCESS_LIST = new AccessList();
-  const result = apply();
-  if (GLOBAL_ACCESS_LIST) GLOBAL_ACCESS_LIST.registerOnChange(onChange);
-  return result;
-}
-
 @Observable
-class Test {
-  a: number = 1;
-  b: string = "b";
-  c: number;
-  constructor(c: number) {
-    this.c = c;
-  }
-  setC(c: number) {
-    this.c = c;
+class Suspect {
+  name: string = "";
+  suspciousness: number = 0;
+  constructor(name: string, suspiciousness: number) {
+    this.name = name;
+    this.suspciousness = suspiciousness;
   }
 }
 
-const t = new Test(9);
+const suspect = new Suspect("Glib Butler", 33);
+const suspect2 = new Suspect("Jimmy the Shrimp", 10);
 withObservationTracking(
   () => {
-    console.log(`Observing property of ${t.c}`);
+    console.log(`Observing property of ${suspect.suspciousness}`); // <-- Only observing property of suspect.suspciousness
   },
   () => {
     console.log("A property has changed");
   }
 );
 
-t.c = 10;
-t.a;
+/**
+ * "suspect.name = "Glib Garlic";" does not trigger onChange callback of withObservationTracking
+ * because "apply" block does NOT contain "suspect.name"
+ * */
+suspect.name = "Glib Garlic";
+console.log(suspect2.name);
+/**
+ * "suspect.suspciousness = 30;" triggers onChange callback of withObservationTracking
+ * because "apply" block contains "suspect.suspciousness"
+ * */
+suspect.suspciousness = 30;
